2117e9113d9560adc82855f3a2d58f74b74c074e
weam1995 <weam_1995@yahoo.com>
2024-03-06 17:55:43 +0100

    added more story

--- src/stories/Counter.stories.ts
+++ src/stories/Counter.stories.ts
@@ -1,14 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Counter from '../components/counter/counter';
 const meta: Meta<typeof Counter> = {
-  title: 'Counter- A customizable react table component',
+  title: 'Counter',
   component: Counter,
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const basicCounter: Story = {
+export const BasicCounter: Story = {
   args: {
     initialValue: 1,
+  },
+};
+export const ComplexCounter: Story = {
+  args: {
+    initialValue: 167662525,
+  },
+};
+export const NegativeCounter: Story = {
+  args: {
+    initialValue: -62,
   },
 };
---
+++ src/stories/SearchForm.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SearchForm from '../components/searchForm/searchForm';
+
+const meta: Meta<typeof SearchForm> = {
+  title: 'SearchForm',
+  component: SearchForm,
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const EmptySearchForm: Story = {
+  args: {
+    initialSearchQuery: '',
+  },
+};
+export const BasicSearchForm: Story = {
+  args: {
+    initialSearchQuery: 'Hello World',
+  },
+};
+export const LongTextSearchForm: Story = {
+  args: {
+    initialSearchQuery:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+};
