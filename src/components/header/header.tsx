d05a8eba0deb3ff9f459bce5fc1747107db0ac54
weam1995 <weam_1995@yahoo.com>
2024-03-13 12:53:22 +0100

    draft 1 added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import SearchForm from '../searchForm/SearchForm';
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   const handleSearch = (inputData: string) => {
     console.log(inputData);
   };
 
+  const addMovieClickHandler = () => {
+    setShowModal(true);
+  };
   return (
     <>
       <div className="header">
+        <button onClick={addMovieClickHandler}>Add Movie</button>
         <SearchForm initialSearchQuery="" onSearch={handleSearch} />
       </div>
-      <div className="boarderRectangle"></div>
+      <div className="border-rectangle"></div>
     </>
   );
 };
---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,12 @@
+import './search-form.css';
+import { useRef, useState } from 'react';
+
+interface ModalProps {
+  title: string;
+  children: HTMLElement;
+  closeModalHandler: Mock<any, void>;
+}
+const Modal = ({ title, children, closeModalHandler }: ModalProps) => {
+  return {};
+};
+export default Modal;
