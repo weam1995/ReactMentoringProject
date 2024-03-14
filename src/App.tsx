96277ea6a199f3223c577b262eb66f8a2085cd26
Weam Mahmoud <weam_mahmoud@epam.com>
2024-03-14 13:43:26 +0100

    modified release date

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     setSelectedSortOption(sortOption);
   };
   const movieClickHandler = (e: React.MouseEvent<HTMLImageElement>) => {};
-
+  const getReleaseYear = (releaseDate:string) => new Date(releaseDate).getFullYear();
   return (
     <div className="page-layout">
       <Counter initialValue={1} />
@@ -48,7 +48,7 @@ function App() {
         <MovieTitle
           imageUrl="https://m.media-amazon.com/images/S/pv-target-images/c8d3558dfe682e8a8c5d797623548e1fc85b45a7bc7a6fa25799940012532f53.jpg"
           movieName="Eat Pray Love"
-          releaseYear={2010}
+          releaseYear={getReleaseYear('2013-12-13')}
           relevantGenres={['Romantic', 'Thrilling']}
           onMovieSelect={movieClickHandler}
         />
@@ -57,7 +57,7 @@ function App() {
       <MovieDetails
         imageUrl="https://m.media-amazon.com/images/S/pv-target-images/c8d3558dfe682e8a8c5d797623548e1fc85b45a7bc7a6fa25799940012532f53.jpg"
         movieName="Eat Pray Love"
-        releaseYear={2010}
+        releaseYear= {getReleaseYear('2016-12-13')}
         rating={8.9}
         duration={260}
         description=' A Married woman realizes how unhappy her marriage really is, and that her life needs to go in a different direction. After a painful divorce, she takes off on a round-the-world journey to "find herself".'
